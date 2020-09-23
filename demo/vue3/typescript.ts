2a8a38adcd3b7704c22d0f6b37a83b2777b3149e
shouhua <shouhua@outlook.com>
2020-09-23 16:31:30 +0800

    feat: ts添加全局变量的自定义属性

--- demo/vue3/typescript.ts
+++ demo/vue3/typescript.ts
@@ -1,3 +1,11 @@
+import { ref } from 'vue'
+
+declare global {
+  interface Window {
+    init: () => void
+  }
+}
+
 interface Job {
   (): void
   id?: number
@@ -8,3 +16,5 @@ const job1: Job = function() {
   console.log('just a job')
 }
 job1.allowRecursive = !!job1
+
+window.init = () => {}
